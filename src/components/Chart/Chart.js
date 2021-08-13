91f277e6a679374b9a890e052b29708d2e114b54
Ryan Zhou <ryanzhou7@gmail.com>
2021-08-13 10:44:51 -0700

    Chart rename

---
+++ src/components/Chart/Chart.js
@@ -0,0 +1,134 @@
+import React, { useRef, useEffect, useState } from "react";
+import { Button, Card, OverlayTrigger, Popover } from "react-bootstrap";
+import { withOrientationChange } from "react-device-detect";
+import Webcam from "react-webcam";
+import * as imageReducer from "../../redux/imageReducer";
+import { useSelector, useDispatch } from "react-redux";
+import Adjuster from "../Adjuster/Adjuster";
+import target from "../../assets/target/circle.png";
+import sample from "../../assets/sample/63.jpg";
+import "./index.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+// For the analysis
+
+function Chart(props) {
+  // Setup
+  const dispatch = useDispatch();
+  const [isCameraOn, setIsCameraOn] = useState(true);
+
+  // Redux
+  const videoConstraints = useSelector(
+    (state) => state.videoReducer.videoConstraints
+  );
+  const image = useSelector((state) => state.image.source);
+  const canvasDimensions = useSelector(
+    (state) => state.canvasSettings.canvasDimensions
+  );
+
+  // Props
+  const { isPortrait } = props;
+
+  // Ref
+  const webcamContainerRef = useRef(null);
+  const webcamRef = useRef(null);
+  const autoAnalyzeContainerRef = useRef(null);
+
+  // Set a default image for debugging bad images
+  useEffect(() => {
+    //dispatch(imageReducer.setImageOnload(sample));
+  }, []);
+
+  const capture = () => {
+    if (!isCameraOn) {
+      setIsCameraOn(true);
+      return;
+    }
+
+    const screenshot = webcamRef.current.getScreenshot();
+    dispatch(imageReducer.setImageOnload(screenshot));
+    dispatch(imageReducer.setImage(screenshot));
+    window.scrollTo(0, autoAnalyzeContainerRef.current.offsetTop);
+
+    setIsCameraOn(false);
+  };
+
+  // Children props setup
+  const adjusterProps = {
+    webcamRef,
+    webcamContainerRef,
+    image: image,
+    canvasDimensions: {
+      canvasWidth: canvasDimensions.width,
+      canvasHeight: canvasDimensions.height,
+    },
+    drawDimensions: {
+      drawWidth: canvasDimensions.width,
+      drawHeight: canvasDimensions.height,
+    },
+    isPortrait,
+    cameraState: [isCameraOn, setIsCameraOn],
+  };
+
+  const popover = (
+    <Popover id="popover-basic">
+      <Popover.Content>
+        Draw your chart with red sharpie. Align the red target's circle
+        concentrically with the chart's smallest circle that still encompasses
+        all of the the drawn lines for increased accuracy. Also ensure the
+        target cross is aligned with the chart. Camera focus will happen
+        automatically. Tap the (?) again to close this popover.
+      </Popover.Content>
+    </Popover>
+  );
+  return (
+    <div className="App mt-2">
+      <h2 className="card-title" style={{ display: "inline" }}>
+        Capture chart{" "}
+      </h2>
+      <OverlayTrigger trigger="click" placement="bottom" overlay={popover}>
+        <FontAwesomeIcon icon="question-circle" size="2x" />
+      </OverlayTrigger>
+      <Card className="mt-4">
+        <div className="mx-auto">
+          <div className="capture-container mx-auto" ref={webcamContainerRef}>
+            {isCameraOn && (
+              <>
+                <Webcam
+                  ref={webcamRef}
+                  audio={false}
+                  height={videoConstraints.height}
+                  screenshotFormat="image/jpeg"
+                  width={videoConstraints.width}
+                  videoConstraints={videoConstraints}
+                />
+                <div className="overlay">
+                  <img
+                    className="target"
+                    //width -20 leaves some padding on the left and right side
+                    style={{ width: videoConstraints.width - 20 }}
+                    src={target}
+                  />
+                </div>
+              </>
+            )}
+          </div>
+        </div>
+        <div className="my-2 z-top mx-auto">
+          <Button className="capture-button" onClick={() => capture()}>
+            <FontAwesomeIcon icon="camera" size="3x" />
+          </Button>
+        </div>
+      </Card>
+
+      <div className="mt-4" ref={autoAnalyzeContainerRef}>
+        <Adjuster {...adjusterProps} />
+      </div>
+      <br />
+      <br />
+      <br />
+    </div>
+  );
+}
+
+export default withOrientationChange(Chart);
--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,134 +1,48 @@
 import React, { useRef, useEffect, useState } from "react";
-import { Button, Card, OverlayTrigger, Popover } from "react-bootstrap";
+import {  Tabs, Tab } from "react-bootstrap";
 import { withOrientationChange } from "react-device-detect";
 import Webcam from "react-webcam";
-import * as imageReducer from "../../redux/imageReducer";
-import { useSelector, useDispatch } from "react-redux";
-import Adjuster from "../Adjuster/Adjuster";
-import target from "../../assets/target/circle.png";
-import sample from "../../assets/sample/63.jpg";
 import "./index.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import Chart from "../Chart/Chart"
+//import Eyes from "../Eyes/Eyes"
+//import Scribe from "../Scribe/Scribe"
 
 // For the analysis
 
-function Auto(props) {
-  // Setup
-  const dispatch = useDispatch();
-  const [isCameraOn, setIsCameraOn] = useState(true);
-
-  // Redux
-  const videoConstraints = useSelector(
-    (state) => state.videoReducer.videoConstraints
-  );
-  const image = useSelector((state) => state.image.source);
-  const canvasDimensions = useSelector(
-    (state) => state.canvasSettings.canvasDimensions
-  );
-
-  // Props
-  const { isPortrait } = props;
-
-  // Ref
-  const webcamContainerRef = useRef(null);
-  const webcamRef = useRef(null);
-  const autoAnalyzeContainerRef = useRef(null);
-
-  // Set a default image for debugging bad images
-  useEffect(() => {
-    //dispatch(imageReducer.setImageOnload(sample));
-  }, []);
-
-  const capture = () => {
-    if (!isCameraOn) {
-      setIsCameraOn(true);
-      return;
-    }
-
-    const screenshot = webcamRef.current.getScreenshot();
-    dispatch(imageReducer.setImageOnload(screenshot));
-    dispatch(imageReducer.setImage(screenshot));
-    window.scrollTo(0, autoAnalyzeContainerRef.current.offsetTop);
-
-    setIsCameraOn(false);
-  };
-
-  // Children props setup
-  const adjusterProps = {
-    webcamRef,
-    webcamContainerRef,
-    image: image,
-    canvasDimensions: {
-      canvasWidth: canvasDimensions.width,
-      canvasHeight: canvasDimensions.height,
-    },
-    drawDimensions: {
-      drawWidth: canvasDimensions.width,
-      drawHeight: canvasDimensions.height,
-    },
-    isPortrait,
-    cameraState: [isCameraOn, setIsCameraOn],
-  };
-
-  const popover = (
-    <Popover id="popover-basic">
-      <Popover.Content>
-        Draw your chart with red sharpie. Align the red target's circle
-        concentrically with the chart's smallest circle that still encompasses
-        all of the the drawn lines for increased accuracy. Also ensure the
-        target cross is aligned with the chart. Camera focus will happen
-        automatically. Tap the (?) again to close this popover.
-      </Popover.Content>
-    </Popover>
-  );
+function Main() {
+  
+  const chart = "Chart";
+  const eyes = "Eyes";
+  const notes = "Notes";
+  const [tab, setTab] = useState(chart);
   return (
-    <div className="App mt-2">
-      <h2 className="card-title" style={{ display: "inline" }}>
-        Capture chart{" "}
-      </h2>
-      <OverlayTrigger trigger="click" placement="bottom" overlay={popover}>
-        <FontAwesomeIcon icon="question-circle" size="2x" />
-      </OverlayTrigger>
-      <Card className="mt-4">
-        <div className="mx-auto">
-          <div className="capture-container mx-auto" ref={webcamContainerRef}>
-            {isCameraOn && (
-              <>
-                <Webcam
-                  ref={webcamRef}
-                  audio={false}
-                  height={videoConstraints.height}
-                  screenshotFormat="image/jpeg"
-                  width={videoConstraints.width}
-                  videoConstraints={videoConstraints}
-                />
-                <div className="overlay">
-                  <img
-                    className="target"
-                    //width -20 leaves some padding on the left and right side
-                    style={{ width: videoConstraints.width - 20 }}
-                    src={target}
-                  />
-                </div>
-              </>
-            )}
-          </div>
-        </div>
-        <div className="my-2 z-top mx-auto">
-          <Button className="capture-button" onClick={() => capture()}>
-            <FontAwesomeIcon icon="camera" size="3x" />
-          </Button>
-        </div>
-      </Card>
+    // <Tabs
+    //   id="controlled-tab-example"
+    //   activeKey={tab}
+    //   onSelect={(k) => setTab(k)}
+    //   className="mb-3 justify-content-center"
+    // >
+    //   <Tab eventKey={chart} title={"Chart"}>
+        
+    //       {/* <Chart selectedTab={tab}/> */}
+        
+    //   </Tab>
+    //   <Tab eventKey={eyes} title={"Eyes"}>
+    //     {tab === eyes &&(
+    //       <Eyes selectedTab={tab}/>)
+    //     }
+    //   </Tab>
+    //   <Tab eventKey={notes} title={"Notes"}>
+    //     {tab === notes &&
+    //       <Scribe selectedTab={tab}/>
+    //     }
+    //   </Tab>
+    // </Tabs>
 
-      <div className="mt-4" ref={autoAnalyzeContainerRef}>
-        <Adjuster {...adjusterProps} />
-      </div>
-      <br />
-      <br />
-      <br />
-    </div>
+    <Chart/>
+
   );
 }
 
-export default withOrientationChange(Auto);
+export default withOrientationChange(Main);
