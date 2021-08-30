d149b16593b27622c539be7aa000cdb2413b7a20
Ryan <ryanzhou7@gmail.com>
2021-08-29 17:14:47 -0700

    Updates

---
+++ precache-manifest.524d67657bdef04bff30951c28638128.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "06121ed5ed45fa652e0df44abaf73331",
+    "url": "/p-calc/index.html"
+  },
+  {
+    "revision": "6ef8f110a46f39d022b6",
+    "url": "/p-calc/static/css/3.20c7418a.chunk.css"
+  },
+  {
+    "revision": "c5ada6317df6ecbb652c",
+    "url": "/p-calc/static/css/main.d51d4023.chunk.css"
+  },
+  {
+    "revision": "fc0d9f0d896b6842dc4d",
+    "url": "/p-calc/static/js/2.7299aaac.chunk.js"
+  },
+  {
+    "revision": "7ec01595672f75e83fd81b41f132f4c1",
+    "url": "/p-calc/static/js/2.7299aaac.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6ef8f110a46f39d022b6",
+    "url": "/p-calc/static/js/3.473d1abd.chunk.js"
+  },
+  {
+    "revision": "c6dcdc64178b87977395391c85eeaa0c",
+    "url": "/p-calc/static/js/3.473d1abd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e5ead1699861931a6a72",
+    "url": "/p-calc/static/js/4.f7332b3e.chunk.js"
+  },
+  {
+    "revision": "c0cd2c5f40481cc6aeff754b29a7cf95",
+    "url": "/p-calc/static/js/4.f7332b3e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f71d1c4420e539a09ef6",
+    "url": "/p-calc/static/js/5.f2de38e3.chunk.js"
+  },
+  {
+    "revision": "54a13f759b1799645c0f4c440498a75c",
+    "url": "/p-calc/static/js/5.f2de38e3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c5ada6317df6ecbb652c",
+    "url": "/p-calc/static/js/main.134dbee4.chunk.js"
+  },
+  {
+    "revision": "35984fa5899cada11ec3",
+    "url": "/p-calc/static/js/runtime-main.8dbce061.js"
+  },
+  {
+    "revision": "8a2e2c1e3413ecdde09522c21fa2ef59",
+    "url": "/p-calc/static/media/63.8a2e2c1e.jpg"
+  },
+  {
+    "revision": "9036c1218bc2546f2d09c2e628af5833",
+    "url": "/p-calc/static/media/circle.9036c121.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/p-calc/precache-manifest.74310d9c350c7c4a3d55a17d6a7a4d88.js"
+  "/p-calc/precache-manifest.524d67657bdef04bff30951c28638128.js"
 );
 
 self.addEventListener('message', (event) => {
